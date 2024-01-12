c970bb34db0cd5d891fa90b0acd96039efc052c3
Avolk01 <volkodavartem01@mail.ru>
2024-01-13 00:09:28 +0300

    feat: init admin js

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,47 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
+const DEFAULT_ADMIN = {
+    email: 'admin@example.com',
+    password: 'password',
+};
+
+const authenticate = async (email: string, password: string) => {
+    if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+        return Promise.resolve(DEFAULT_ADMIN);
+    }
+    return null;
+};
+
 @Module({
     imports: [
+        // AdminJS version 7 is ESM-only. In order to import it, you have to use dynamic imports.
+        import('@adminjs/nestjs').then(({ AdminModule }) =>
+            AdminModule.createAdminAsync({
+                useFactory: () => ({
+                    adminJsOptions: {
+                        rootPath: '/admin',
+                        resources: [],
+                    },
+                    auth: {
+                        authenticate,
+                        cookieName: 'adminjs',
+                        cookiePassword: 'secret',
+                    },
+                    sessionOptions: {
+                        resave: true,
+                        saveUninitialized: true,
+                        secret: 'secret',
+                    },
+                }),
+            }),
+        ),
         ConfigModule.forRoot({
             envFilePath: '.env.development',
             isGlobal: true,
         }),
     ],
+
     controllers: [AppController],
     providers: [AppService],
 })
